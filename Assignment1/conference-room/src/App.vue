9a007fa9fd7d1d03261d3cac4143e5891b1f7090
sustechkl <12111012@mail.sustech.edu.cn>
2023-09-19 22:06:49 +0800

    Roughly finished

---
+++ Assignment1/conference-room/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <ConferenceRoomTable />
+</template>
+
+<script>
+import ConferenceRoomTable from './components/ConferenceRooomTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ConferenceRoomTable
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
